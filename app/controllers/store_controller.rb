d75eab9d94b26eb2c058c8e26c77421eff5db1de
Julie Torres <reeddetorres@gmail.com>
2015-04-25 23:22:13 -0400

    Add price to line items

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,4 +2,8 @@ class StoreController < ApplicationController
   def index
     @products = Product.all
   end
+
+  def count_index_visits
+    session[:counter] ? session[:counter] += 1 : session[:counter] = 1
+  end
 end
